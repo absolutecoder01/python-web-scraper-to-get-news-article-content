3a6b0c8bde796c016975fdef3dfd3ac270305577
absolute <156543239+absolutecoder01@users.noreply.github.com>
2024-01-27 19:15:16 +0100

    Added ability to save file to txt and refactoring

--- main.py
+++ main.py
@@ -1,23 +1,63 @@
 import requests
+import os
+import shutil
 from bs4 import BeautifulSoup
+from newspaper import Article
 
-url = input('Enter the URL to connect')
+FILE_NAME = "data.txt"
+DOWNLOAD_PATH = os.path.join(os.path.expanduser("~"), "Downloads")
+DESTINATION_PATH = os.path.join(DOWNLOAD_PATH, FILE_NAME)
 
-#https://www.washingtonpost.com/technology/2020/09/25/privacy-check-blacklight/
-#https://edition.cnn.com/travel/article/scenic-airport-landings-2020/index.html
-#https://www.reuters.com/article/us-health-coronavirus-global-deaths/global-coronavirus-deaths-pass-agonizing-milestone-of-1-million-idUSKBN26K08Y
+# A counter that count the number of saved files
+counter = 1
 
-r = requests.get(url)
-status = r.status_code
-#Check status of the connection
-if status != 200:
-  print("Failed to connect website or wrong url")
-else:
-  print("Succesfully Connected")
 
-#Connect BeautifulSoup html parser
-soup = BeautifulSoup(r.content, 'html.parser')
+# A function that will save data to txt file
+def save(data_to_save):
+  global DESTINATION_PATH, counter
 
-# Getting the title tag
-print(soup.title)
-# return the title and content of the article to the user.
+  if os.path.exists(DESTINATION_PATH):
+    file_name, file_extension = os.path.splitext(DESTINATION_PATH)
+    file_name_with_counter = f"{file_name}({counter}){file_extension}"
+    DESTINATION_PATH = os.path.join(DOWNLOAD_PATH, file_name_with_counter)
+    counter += 1
+
+  with open(FILE_NAME, "w") as file:
+    file.write(data_to_save)
+
+  shutil.move(FILE_NAME, DESTINATION_PATH)
+  print(f"File saved in {DESTINATION_PATH}")
+
+
+#Main function
+def main():
+  url = input('Enter the URL to connect: ')
+  r = requests.get(url)
+  status = r.status_code
+  check_status(status, url)
+
+
+#Function to check if the status code is 200
+def check_status(status, url):
+  if status != 200:
+    print("Failed to connect website or wrong url")
+  else:
+    print("Succesfully Connected")
+    get_content_from_page(url)
+
+
+#Function to get content from page
+def get_content_from_page(url):
+  article = Article(url)
+  article.download()
+  article.parse()
+
+  # Get the article text:
+  print(article.text)
+  #Save page content to file
+  main_content = article.text
+  save(main_content)
+
+
+if __name__ == "__main__":
+  main()
